4e2de97dfb1ee8434dac0679d04c21bfc259c381
jessica-slee4275 <jessica.slee4275@gmail.com>
2020-02-12 00:35:25 -0500

    first commit

---
+++ lbcit_JessicaLee/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>lbcit - Jessica Lee</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+        <table id="mytable" class="table table-condensed">
+            <thead>
+                <tr>
+                    <th>Month</th>
+                    <%foreach (var s in Series) {%> 
+                        <th><%= s.Key %></th>
+                    <%} %>
+                </tr>
+            </thead>
+            <tbody>
+                <%foreach (string month in Categories) {%>
+                    <tr>
+                        <td><%= month %></td>
+                        <td id ="country"></td>
+                    </tr>
+                <%  } %>
+
+               
+            </tbody>
+        </table>
+        </div>
+    </form>
+    <script src="//code.jquery.com/jquery.min.js"></script>
+    <script>
+
+       <%-- <%foreach (var s in Series) {%> 
+            <%foreach (var data in s.Value) {%> 
+                $('#country > td:last').append('<td><%= data %></td>');
+            <%  } %>
+        <%  } %>--%>
+    </script>
+</body>
+</html>
---
+++ lbcit_JessicaLee/Default.aspx.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //private List<float> data = new List<float>() { };
+    //public List<float> Data { get { return data; } }
+    private List<string> categories = new List<string>() { };
+    public List<string> Categories { get { return categories; } }
+
+    private List<float> data = new List<float>() { };
+    public List<float> Data { get { return data; } }
+
+    private Dictionary<string, List<float>> series = new Dictionary<string, List<float>>() { };
+    public Dictionary<string, List<float>> Series { get { return series; } }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string json = this.Request_Json();
+        this.ParseJson(json);
+    }
+    private string Request_Json()
+    {
+        string result = null;
+        string url = "https://www.lbcit.ca/demo/api";
+        string keyVal = "d9658b9d-4f86-491f-bd67-86af0c547a5c";
+        url = $"{url}/?key={keyVal}";
+        try
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            result = reader.ReadToEnd();
+            stream.Close();
+            response.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return result;
+    }
+    private void ParseJson(String json)
+    {
+        JObject obj = JObject.Parse(json);
+        
+        JArray categoriesArr = JArray.Parse(obj["categories"].ToString());
+        JArray seriesArr = JArray.Parse(obj["series"].ToString());
+        foreach (var itemObj in categoriesArr)
+        {
+            categories.Add(itemObj.ToString());
+        }
+        foreach (JObject itemObj in seriesArr)
+        {
+            //data.Add(itemObj["name"].ToString());
+            JArray dataArr = JArray.Parse(itemObj["data"].ToString());
+            foreach (var dataEle in dataArr)
+            {
+                //series.Add(itemObj["name"].ToString(), dataEle);
+                data.Add(float.Parse(dataEle.ToString()));
+            }
+            series.Add(itemObj["name"].ToString(), data);
+            //JArray dataArr = JArray.Parse(obj["data"].ToString());
+            //data.Add(float.Parse(itemObj["data"].ToString()));
+            //series.Add(itemObj["name"].ToString());
+            //JArray dataArr = JArray.Parse(itemObj["data"].ToString());
+            //foreach (var ele in dataArr)
+            //{
+            //    issue.Done = ele.ToString();
+            //}
+            //issue.Author = itemObj["author"]["name"].ToString();
+            //issues.Add(issue);
+        }
+
+        //IssueListView.ItemsSource = issues;
+    }
+}
